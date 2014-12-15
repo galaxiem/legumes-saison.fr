0a93bece9a1352d75f911646d3a5d2c3ac6b51de
Nicolas Peltier <peltier.nicolas@gmail.com>
2014-12-15 23:23:10 +0100

    hover sur les listes

--- www/js/legume.js
+++ www/js/legume.js
@@ -9,14 +9,21 @@ var scale = function(evt) {
 		img.width(newWidth);
 		img.css("margin-left", (w - newWidth)/ 2);
 	}
+}, clique = function(elt) {
+	document.location.href = $(elt).find("a").attr("href");
+	return false;
 }
+	
+
 $(document).on("ready", function() {
 	$(".banner").click(function(event) {
 		document.location.href = "/";
 		return false;
 	});
 	$("h4.petit-rond").click(function(event) {
-		document.location.href = $(this).find("a").attr("href");
-		return false;
+		return clique(this);
+	});
+	$("ul.liste-legume > li").click(function(event) {
+		return clique(this);
 	});
 });
